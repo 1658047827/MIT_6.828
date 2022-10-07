d91637d1fac87b2b8acacaf7fb75862979a4bd3e
TomFish <1658047827@qq.com>
2022-10-07 14:19:00 +0800

    format

--- kern/trap.c
+++ kern/trap.c
@@ -87,24 +87,25 @@ trap_init(void)
     SETGATE(idt[T_DIVIDE], 0, GD_KT, t_divide, 0);
     SETGATE(idt[T_DEBUG], 0, GD_KT, t_debug, 0);
     SETGATE(idt[T_NMI], 0, GD_KT, t_nmi, 0);
-    SETGATE(idt[T_BRKPT], 0, GD_KT, t_brkpt, 3);    // USER PRIVILEGE
+    SETGATE(idt[T_BRKPT], 0, GD_KT, t_brkpt, 3);    	// USER PRIVILEGE
     SETGATE(idt[T_OFLOW], 0, GD_KT, t_oflow, 0);
     SETGATE(idt[T_BOUND], 0, GD_KT, t_bound, 0);
     SETGATE(idt[T_ILLOP], 0, GD_KT, t_illop, 0);
     SETGATE(idt[T_DEVICE], 0, GD_KT, t_device, 0);
     SETGATE(idt[T_DBLFLT], 0, GD_KT, t_dblflt, 0);
+
     SETGATE(idt[T_TSS], 0, GD_KT, t_tss, 0);
     SETGATE(idt[T_SEGNP], 0, GD_KT, t_segnp, 0);
     SETGATE(idt[T_STACK], 0, GD_KT, t_stack, 0);
     SETGATE(idt[T_GPFLT], 0, GD_KT, t_gpflt, 0);
     SETGATE(idt[T_PGFLT], 0, GD_KT, t_pgflt, 0);
+
     SETGATE(idt[T_FPERR], 0, GD_KT, t_fperr, 0);
     SETGATE(idt[T_ALIGN], 0, GD_KT, t_align, 0);
     SETGATE(idt[T_MCHK], 0, GD_KT, t_mchk, 0);
     SETGATE(idt[T_SIMDERR], 0, GD_KT, t_simderr, 0);
 
-
-	// Per-CPU setup 
+	// Per-CPU setup
 	trap_init_percpu();
 }
 
