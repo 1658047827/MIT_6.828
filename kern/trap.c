4aa8131b14b9dd0cd78f0d7ce3fc59d395d06945
TomFish <1658047827@qq.com>
2022-10-18 19:47:21 +0800

    finish trap_init_percpu

--- kern/trap.c
+++ kern/trap.c
@@ -156,21 +156,23 @@ trap_init_percpu(void)
 	// user space on that CPU.
 	//
 	// LAB 4: Your code here:
-
+	// 发现原本的代码可以重用，只需要利用thiscpu->cpu_ts和cpunum()
+	// 然后注意设置好对应的虚拟地址即可
+	size_t i = cpunum();
 	// Setup a TSS so that we get the right stack
 	// when we trap to the kernel.
-	ts.ts_esp0 = KSTACKTOP;
-	ts.ts_ss0 = GD_KD;
-	ts.ts_iomb = sizeof(struct Taskstate);
+	thiscpu->cpu_ts.ts_esp0 = KSTACKTOP-i*(KSTKSIZE+KSTKGAP);
+	thiscpu->cpu_ts.ts_ss0 = GD_KD;
+	thiscpu->cpu_ts.ts_iomb = sizeof(struct Taskstate);
 
 	// Initialize the TSS slot of the gdt.
-	gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts),
+	gdt[(GD_TSS0 >> 3) + i] = SEG16(STS_T32A, (uint32_t) (&thiscpu->cpu_ts),
 					sizeof(struct Taskstate) - 1, 0);
-	gdt[GD_TSS0 >> 3].sd_s = 0;
+	gdt[(GD_TSS0 >> 3) + i].sd_s = 0;
 
 	// Load the TSS selector (like other segment selectors, the
 	// bottom three bits are special; we leave them 0)
-	ltr(GD_TSS0);
+	ltr(GD_TSS0 + (i<<3));  // 这里要注意段选子0~2bit是标志位，3bit开始才是index
 
 	// Load the IDT
 	lidt(&idt_pd);
